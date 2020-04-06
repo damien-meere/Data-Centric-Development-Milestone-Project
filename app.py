dfa9121e1bcf4a74416a34a8f47ec54a8a4860bf
Damien Meere <damien.meere@bt.com>
2020-04-06 13:12:42 +0000

    Edit functionality built and wired for categories, course sizes and course duration

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ app.config["MONGO_URI"] = os.getenv('MONGO_URI_MILESTONE3', 'mongodb://localhost
 
 mongo = PyMongo(app)
 
+# Course Related CRUD Functionality
+
 @app.route('/')
 @app.route('/get_courses')
 def get_courses():
@@ -55,6 +57,64 @@ def edit_course(course_id):
         'max_subscriber': request.form.get('max_subscriber')
     })
     return redirect(url_for('get_courses'))
+
+@app.route('/delete_course/<course_id>')
+def delete_course(course_id):
+    mongo.db.courses.remove({'_id': ObjectId(course_id)})
+    return redirect(url_for('get_courses'))
+
+# Category Related CRUD Functionality
+
+@app.route('/get_categories')
+def get_categories():
+    return render_template("categories.html", categories=mongo.db.categories.find())
+
+@app.route('/edit_category/<category_id>')
+def edit_category(category_id):
+    return render_template('editcategory.html',
+    category=mongo.db.categories.find_one({'_id': ObjectId(category_id)}))
+
+@app.route('/update_category/<category_id>', methods=['POST'])
+def update_category(category_id):
+    mongo.db.categories.update(
+        {'_id': ObjectId(category_id)},
+        {'category_name': request.form.get('category_name')})
+    return redirect(url_for('get_categories'))
+
+# Course Duration Related CRUD Functionality
+
+@app.route('/get_durations')
+def get_durations():
+    return render_template("durations.html", durations=mongo.db.course_duration.find())
+
+@app.route('/edit_duration/<duration_id>')
+def edit_duration(duration_id):
+    return render_template('editduration.html',
+    duration=mongo.db.course_duration.find_one({'_id': ObjectId(duration_id)}))
+
+@app.route('/update_duration/<duration_id>', methods=['POST'])
+def update_duration(duration_id):
+    mongo.db.course_duration.update(
+        {'_id': ObjectId(duration_id)},
+        {'duration': request.form.get('duration')})
+    return redirect(url_for('get_durations'))
+
+# Course Size Related CRUD Functionality
+@app.route('/get_sizes')
+def get_sizes():
+    return render_template("sizes.html", sizes=mongo.db.course_sizes.find())
+
+@app.route('/edit_size/<size_id>')
+def edit_size(size_id):
+    return render_template('editsize.html',
+    size=mongo.db.course_sizes.find_one({'_id': ObjectId(size_id)}))
+
+@app.route('/update_size/<size_id>', methods=['POST'])
+def update_size(size_id):
+    mongo.db.course_sizes.update(
+        {'_id': ObjectId(size_id)},
+        {'max_subscriber': request.form.get('max_subscriber')})
+    return redirect(url_for('get_sizes'))
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP', "0.0.0.0"),
